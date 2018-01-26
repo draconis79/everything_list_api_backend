5dd40c37803f447e906fcf76a76aee1364e07286
draconis79 <mandy.tong.79@gmail.com>
2018-01-25 23:58:55 -0800

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,13 @@ class UsersController < ApplicationController
   # GET / users
   def index
     @users = User.all
-    render json: @users
+    render json: @users.to_json(include: :list_categories)
   end
 
 
   # GET /users/1
   def show
-    render json: @user
+    render json: @user.to_json(include: :list_categories)
   end
 
 
