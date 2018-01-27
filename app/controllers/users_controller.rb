ad250c6f5e89e9ccb979466d2bee9ac8eb3bd451
draconis79 <mandy.tong.79@gmail.com>
2018-01-27 09:58:19 -0800

    set up authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-
+  before_action :authenticate_token, except: [:login, :create, :show]
+  before_action :authorize_user, except: [:login, :create, :index, :show]
 
   # GET / users
   def index
@@ -37,13 +38,36 @@ end
   end
 
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  def login
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      token = create_token(user.id, user.username)
+      render json: { status: 200, token: token, user: user }
+    else
+      render json: { status: 401, message: 'Unauthorized' }
+    end
+end
 
 
   private
+
+  def create_token(id, username)
+      JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+  end
+
+    def payload(id, username)
+      {
+        exp: (Time.now + 800.minutes).to_i,
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          username: username
+        }
+      }
+    end
+
+
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
@@ -51,6 +75,6 @@ end
 
   # USER REQUIREMENT
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      params.require(:user).permit(:username, :password_digest, :password)
     end
 end
