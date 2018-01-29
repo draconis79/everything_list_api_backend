1faf60d0082d0f30267a98e3940c94296a39e5ef
draconis79 <mandy.tong.79@gmail.com>
2018-01-28 22:38:58 -0800

    fixed missing : for list categories causing undefined variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :list_categories
-  has_many :list_items, through: list_categories
+  has_many :list_items, through: :list_categories
 end
