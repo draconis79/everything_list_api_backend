dfc70ea343d7659e62cc6a50c93fb74f57c3a421
draconis79 <mandy.tong.79@gmail.com>
2018-01-29 16:38:37 -0800

    updated list items routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :list_categories, only: [:create, :destroy, :update, :show]
-  resources :list_items
+  resources :list_categories, only: [:index, :create, :destroy, :update, :show]
+  resources :list_items, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:show, :create, :update] do
