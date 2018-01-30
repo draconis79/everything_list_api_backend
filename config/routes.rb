9ca93354bbc93ae2ec2f5874eda844e2ce66a1c3
draconis79 <mandy.tong.79@gmail.com>
2018-01-29 20:06:51 -0800

    added list and task models, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :tasks, only: [:index, :create, :update, :destroy, :show]
-  resources :categories, only: [:index, :create, :update, :destroy, :show] do
+  resources :lists, only: [:index, :create, :update, :destroy, :show] do
   end
 end
