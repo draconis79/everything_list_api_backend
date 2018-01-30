56ec8e33fb278f8740edc873dd4037d2cb5e0a54
draconis79 <mandy.tong.79@gmail.com>
2018-01-29 21:22:42 -0800

    reset database

--- db/migrate/20180130023550_create_lists.rb
+++ db/migrate/20180130023550_create_lists.rb
@@ -1,6 +1,10 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
+      t.string :name
+      t.string :task
+
+      t.timestamps
     end
   end
 end
